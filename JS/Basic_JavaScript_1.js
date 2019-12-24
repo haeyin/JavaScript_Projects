e087365f9d4377e9a898227a196e5b17fb24c62b
Clare Shin <haeyin.shin@gmail.com>
2019-12-24 15:54:43 -0800

    Basic Javascript
    basic Javascript test file

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,15 @@
+var X = "Woohoo!";
+document.write(X);
+window.alert(X);
+document.write("<br>\"This is how you write quotes.\"<br>");
+document.write("\"This,\" he said, " + "\"is how you concatenate a string.\"<br>");
+var Y = "This is" + " a very simple equation."
+document.write(Y);
+var Pets = "Ze Animals", Dog = "Deng Deng", Cat = "Mr.Boots", Rabbit = "Snuffles";
+var Pets = Pets.fontcolor("black"), Dog = Dog.fontcolor("brown"), Cat = Cat.fontcolor("orange"), Rabbit = Rabbit.fontcolor("ivory");
+document.write("<br>" + Cat + "<br>");
+
+var world = "What a wonderful world.";
+var world = world.fontcolor("green");
+document.write(world);
+'z' + 'sigmund' /*Not sure how expressions work yet*/
