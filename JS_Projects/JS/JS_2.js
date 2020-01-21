662cef2820512a1af272c4dd87df6f2c92a5be48
Clare Shin <haeyin.shin@gmail.com>
2020-01-21 01:28:13 -0800

    Challenges
    Challenges.

--- JS_Projects/JS/JS_2.js
+++ JS_Projects/JS/JS_2.js
@@ -1,4 +1,37 @@
 function validateForm() {
-    var x = document.forms["Contact"]["firstname"]["lastname"]["email"]["phone"].value;
+    var first = document.forms["Contact"]["firstname"].value;
+    if (first == "") {
+        alert("First name must be filled out");
+        return false;
+    }
+    var last = document.forms["Contact"]["lastname"].value;
+    if (last == "") {
+        alert("Last name must be filled out");
+        return false;
+    }
+    var email = document.forms["Contact"]["email"].value;
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    var phone = document.forms["Contact"]["phone"].value;
+    if (phone == "") {
+        alert("Phone number must be filled out");
+        return false;
+    } else if (isNaN(phone)) {
+        alert("Phone number must be digits only");
+        return false;
+    }
 }
 
+function openForm() {
+    document.getElementById("formception").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("formception").style.display = "none";
+}
+
+function opnfrm() {
+    document.getElementById("fmcptn").style.display = "block";
+}
---
+++ JS_Projects/JS/JS_4.js
@@ -0,0 +1,7 @@
+function openform() {
+    document.getElementById("form").style.display = "block";
+}
+
+function closeform() {
+    document.getElementById("form").style.display = "none";
+}
